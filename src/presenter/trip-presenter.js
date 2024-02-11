c1f272c07ca5d1a10278ae95cb9d198ac480d481
Ve1heora <fareast.vladivostok.chuchalin@gmail.com>
2024-02-11 21:45:12 +1000

    правки

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,24 +1,25 @@
 import AddNewPointView from '../view/add-new-point-view.js';
 import EditPointView from '../view/edit-point-view.js';
-import NewFilterView from '../view/filter-view.js';
+import FilterView from '../view/filter-view.js';
 import PointView from '../view/point-view.js';
-import NewSortView from '../view/sort-view.js';
+import SortView from '../view/sort-view.js';
 import {render, RenderPosition} from '../render.js';
 
 const POINTS_COUNT = 3;
-
-const filtersContainer = document.querySelector('.trip-controls__filters');
-const eventsContainer = document.querySelector('.trip-events');
-
 export default class TripPresenter {
+  constructor({filtersContainer, eventsContainer}) {
+    this.filtersContainer = filtersContainer;
+    this.eventsContainer = eventsContainer;
+  }
+
   init() {
-    render(new EditPointView(), eventsContainer, RenderPosition.AFTERBEGIN);
-    render(new AddNewPointView(), eventsContainer);
-    render(new NewSortView(), eventsContainer);
-    render(new NewFilterView(), filtersContainer);
+    render(new EditPointView(), this.eventsContainer, RenderPosition.AFTERBEGIN);
+    render(new AddNewPointView(), this.eventsContainer);
+    render(new SortView(), this.eventsContainer);
+    render(new FilterView(), this.filtersContainer);
 
     for (let i = 0; i < POINTS_COUNT; i++) {
-      render(new PointView(), eventsContainer);
+      render(new PointView(), this.eventsContainer);
     }
   }
 }
